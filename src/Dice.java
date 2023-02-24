93f9ac2f0cd520bbbe7b2f56bf14a2aa201df487
Linh Dinh <nhlinh1803@gmail.com>
2023-02-23 17:13:48 -0800

    Completed Java Lab 005

---
+++ src/Dice.java
@@ -0,0 +1,25 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Linh Dinh
+ *
+ * @since Version 1.0
+ *
+ */
+import java.util.Random;
+
+public class Dice {
+    private int sideFacingUp;
+    private int sides;
+    private Random randomGenerator;
+    public Dice(int sides){
+        this.sides = sides;
+        this.randomGenerator = new Random();
+    }
+    public void roll(){
+        this.sideFacingUp = this.randomGenerator.nextInt(this.sides) + 1;
+    }
+    public int view(){
+        return this.sideFacingUp;
+    }
+}
--- src/Game.java
+++ src/Game.java
@@ -1,14 +1,39 @@
+/**
+ *
+ * @author Trevor Hartman
+ * @author Linh Dinh
+ *
+ * @since Version 1.0
+ *
+ */
 import java.util.Scanner;
 
 public class Game {
     Player p1;
     Player p2;
     Dice die;
+    public Game(Player p1, Player p2, Dice die){
+        this.p1 = p1;
+        this.p2 = p2;
+        this.die = die;
+    }
 
     public void play() {
+        Player current = this.p1;
+        takeTurn(current);
+        current = nextPlayer(current);
+        takeTurn(current);
+        System.out.println(announceWinner());
+
     }
 
     public Player nextPlayer(Player current) {
+        if(current == this.p1){
+            return this.p2;
+        } else {
+            return this.p1;
+        }
+
     }
 
     public void takeTurn(Player player) {
@@ -16,8 +41,29 @@ public class Game {
     }
 
     public String announceWinner() {
+        System.out.println(p1.getName() + " scored " + p1.getScore() + " points.");
+        System.out.println(p2.getName() + " scored " + p2.getScore() + " points.");
+
+        if (p1.getScore() > p2.getScore()) {
+            return "The winner is " + p1.getName();
+        } else if (p2.getScore() > p1.getScore()) {
+           return "The winner is " + p2.getName();
+        } else {
+            return "It's a tie!";
+        }
+
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number of sides: ");
+        int sides = input.nextInt();
+        Dice die = new Dice(sides);
+        Player p1 = new Player("Player 1");
+        Player p2 = new Player("Player 2");
+        Game game = new Game(p1, p2, die);
+        game.play();
+
+
     }
 }
